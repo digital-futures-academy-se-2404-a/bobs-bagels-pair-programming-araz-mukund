1e97c9bcda0792dd368977d7a7f72a241810b235
Araz <arazabedi@hotmail.com>
2024-04-22 16:07:17 +0100

    test 1 for oop tests added

---
+++ oop-test/test1.js
@@ -0,0 +1,71 @@
+import { assertEquals } from "./testing-framework.js";
+import Basket from "../src2/basket-class.js";
+import Item from  "../src2/item-class.js"
+
+// AFTER EACH Function
+const afterEach = () => {
+    expected = undefined;
+    actual = undefined;
+    result = undefined;
+    testItem = undefined;
+    basket.basketItems = [];
+};
+
+//? TEST 1
+//* Add item to basket using addItem and expect array (basketItems) has increased in length by 1
+
+console.log(`Test 1`);
+console.log(`==================`);
+console.log(
+    `Add item to basket using addItem and expect array (basketItems) has increased in length by 1`
+);
+// Arrange
+const basket = new Basket()
+let expected = basket.getBasketLength() + 1
+let testItem = new Item();
+let actual, result;
+
+// Act
+basket.addItem(testItem);
+actual = basket.getLength();
+// Assert
+result = assertEquals(actual, expected);
+
+// Report
+console.log("\x1b[31m%s\x1b[0m", "This text will be red.");
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+console.log(`==================`);
+
+// Clean Up
+afterEach();
+
+//! END OF TEST 1
+
+//? TEST 2
+//* Test that item passed to addItem is actually added to the basket
+
+console.log(`Test 2`);
+console.log(`==================`);
+console.log(`Test that item passed to addItem is actually added to the basket`);
+// Arrange
+//! testItem is defined in the test setup for test 1
+basket = new Basket();
+expected = true;
+testItem = new testItem();
+
+// Act
+basket.addItem(testItem);
+actual = basket.getBasketItems().includes(testItem);
+
+// Assert
+result = assertEquals(actual, expected);
+
+// Report
+console.log("\x1b[31m%s\x1b[0m", "This text will be red.");
+!result && console.log(`Expected: ${expected}; Actual: ${actual}`);
+console.log(`==================`);
+
+// Clean Up
+afterEach();
+
+//! END OF TEST 2
